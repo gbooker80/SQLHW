090f049573c088da8162df20b48c7fa004565f1b
geoff567 <31115482+geoff567@users.noreply.github.com>
2018-01-18 13:02:47 -0800

    Updated

--- SQLHW.sql
+++ SQLHW.sql
@@ -1,9 +1,9 @@
 USE SAKILA;
 SHOW TABLES;
 
--- Questions
+-- QUESTIONS
 
--- Question 1a
+-- Question 1a:
 SELECT first_name,last_name
 FROM actor;
 
@@ -16,3 +16,14 @@ WHERE first_name like "Joe";
 SELECT first_name, last_name, actor_id
 FROM actor
 WHERE last_name like "%GEN%";
+
+-- Question 2c:
+SELECT first_name, last_name, actor_id
+FROM actor
+WHERE last_name like "%LI%"
+ORDER BY last_name, first_name;
+
+-- Question 2d:
+SELECT country_id, country
+FROM country
+WHERE country in ('Afghanistan','Bangladesh','China')
